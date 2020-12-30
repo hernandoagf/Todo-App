8e206e6f1e937a2c064bb512c45a84eb08eb1009
hernandoagf <22449631+hernandoagf@users.noreply.github.com>
2020-12-30 14:29:49 -0300

    Done: toggle completed and delete functionality

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,11 @@
 import cross from '../icons/icon-cross.svg'
 import checkImg from '../icons/icon-check.svg'
 
-const TodoItem = ({ todo: { task, completed } }) => {
+const TodoItem = ({ todo: { task, completed }, handleClick }) => {
   const checkIcon = <img src={checkImg} alt="check-icon"/>
   
   return (
-    <div className="card-item list">
+    <div className="card-item list" onClick={(e) => handleClick(task, e)}>
       <div className={completed ? 'check-circle completed' : 'check-circle'}>
         {completed ? checkIcon : ''}
       </div>
